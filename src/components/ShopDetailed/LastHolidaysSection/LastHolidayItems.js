9deadf22253d59aff75058fe19e4481d152f1a29
AhamedMinhaj456 <139200872+AhamedMinhaj456@users.noreply.github.com>
2024-06-27 14:52:26 +0530

    Shop home page

--- src/components/ShopDetailed/LastHolidaysSection/LastHolidayItems.js
+++ src/components/ShopDetailed/LastHolidaysSection/LastHolidayItems.js
@@ -7,19 +7,19 @@ let LastHolidaysItems = [
         id: 1,
         itemImage: LondonImg,
         itemTitle: 'Full Services',
-        itemDescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque officia qui voluptatibus doloribus corporis ipsa rem adipisci nesciunt dolorum deleniti?'
+        itemDescription: 'Experience top-notch bike maintenance with our comprehensive services, including tune-ups, brake adjustments, gear optimization, and full overhauls to keep your ride smooth and safe.'
     },
     {
         id: 2,
         itemImage: DubaiImg,
         itemTitle: 'Custom Services',
-        itemDescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque officia qui voluptatibus doloribus corporis ipsa rem adipisci nesciunt dolorum deleniti?'
+        itemDescription: 'Experience personalized cycling perfection with our custom bike services, including tailored fittings, specialized components, and unique modifications to match your exact needs and preferences.'
     },
     {
         id: 3,
         itemImage: ParisImg,
         itemTitle: 'Repair',
-        itemDescription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque officia qui voluptatibus doloribus corporis ipsa rem adipisci nesciunt dolorum deleniti?'
+        itemDescription: 'Get back on the road quickly with our expert bike repair services, addressing everything from minor fixes to major overhauls to ensure your ride is smooth and safe.'
     }
 ];
 
