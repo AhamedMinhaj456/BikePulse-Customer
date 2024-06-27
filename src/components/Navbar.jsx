a9aa62b56c5bccdb28544b9b8f7574e746842e5a
AhamedMinhaj456 <139200872+AhamedMinhaj456@users.noreply.github.com>
2024-06-27 09:47:40 +0530

    protected route/

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,9 @@ import { ReactComponent as ProfileIcon } from '../assets/profile.svg';
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import { useDispatch } from "react-redux";
-import { clearCustomerId, clearCustomerStatus } from "../Slices/CustomerSlice";
-
 
 function Navbar() {
     const navigate = useNavigate();
-    const dispatch = useDispatch();
     const customerStatus = useSelector((state) => state.customerStatus);
     const customerId = useSelector((state) => state.customers);
     const [customer, setCustomer] = useState(null);
@@ -25,8 +21,8 @@ function Navbar() {
     const handleLogout = () => {
         // Implement logout logic here, e.g., clear localStorage, dispatch logout action, etc.
         // For demonstration purposes, let's clear customerId and customerStatus in localStorage
-        dispatch(clearCustomerId());
-        dispatch(clearCustomerStatus());
+        localStorage.removeItem('customerId');
+        localStorage.removeItem('customerStatusId');
         // Redirect to login or home page after logout
         navigate("/");
     };
@@ -50,19 +46,19 @@ function Navbar() {
 
     return (
         <div className="navbar-container">
-            <div className="navbar-logo">
+            <Link to="/" className="navbar-logo">
                 <img src={logoImage} alt="Logo" />
                 <div className="brand-text">BikePulse</div>
-            </div>
+            </Link>
             <div className="navbar-links">
-                <Link to="/" className="nav-link home-link">Home</Link>
-                <Link to="/aboutus" className="nav-link about-link">About us</Link>
-                <Link to="/shops" className="nav-link shops-link">Shops</Link>
+                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/aboutus" className="nav-link">About us</Link>
+                <Link to="/shops" className="nav-link">Shops</Link>
                 {customerStatus ? (
                     <div className="nav-link-container">
                         {customer ? (
                             <>
-                            <div onClick={handleUserIconClick} className="profile-container">
+                                <div onClick={handleUserIconClick} className="profile-container">
                                     <ProfileIcon className="profile" />
                                     <span className="customer-name">{customer.customerName}</span>
                                 </div>
